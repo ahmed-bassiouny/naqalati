8c0462b3908b86866d2f6ee3e8a87ca9686a5e22
bassiouny <ahmedbassiouny26@gmail.com>
2017-11-10 14:28:20 +0200

    init

---
+++ app/src/main/java/com/ntamtech/naqalati/activities/SigninActivity.java
@@ -0,0 +1,15 @@
+package com.ntamtech.naqalati.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ntamtech.naqalati.R;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
