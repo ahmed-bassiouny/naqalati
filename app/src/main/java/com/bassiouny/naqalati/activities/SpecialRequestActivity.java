1032c70d38bceaaf7dff4676619e5c3ccc2a7d07
bassiouny <ahmedbassiouny26@gmail.com>
2017-11-20 19:38:30 +0200

    change logo

--- app/src/main/java/com/bassiouny/naqalati/activities/SpecialRequestActivity.java
+++ app/src/main/java/com/bassiouny/naqalati/activities/SpecialRequestActivity.java
@@ -13,34 +13,55 @@ public class SpecialRequestActivity extends AppCompatActivity implements View.On
 
 
     private TextInputEditText etName;
-    private TextInputEditText etPhone;
+    private TextInputEditText etAddress;
+    private TextInputEditText etEmail;
     private TextInputEditText requestType;
-    private TextInputEditText requestDetails;
+    private TextInputEditText size;
+    private TextInputEditText startpoint;
+    private TextInputEditText endpoint;
+    private TextInputEditText pay;
+    private TextInputEditText number;
+    private TextInputEditText cartype;
+    private TextInputEditText numberofcar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_special_request);
         etName = findViewById(R.id.et_name);
-        etPhone = findViewById(R.id.et_phone);
+        etAddress = findViewById(R.id.et_address);
+        etEmail = findViewById(R.id.et_email);
         requestType = findViewById(R.id.request_type);
-        requestDetails = findViewById(R.id.request_details);
+        size = findViewById(R.id.size);
+        startpoint = findViewById(R.id.startpoint);
+        endpoint = findViewById(R.id.endpoint);
+        pay = findViewById(R.id.pay);
+        number = findViewById(R.id.number);
+        cartype = findViewById(R.id.cartype);
+        numberofcar = findViewById(R.id.numberofcar);
         findViewById(R.id.send).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if(etName.getText().toString().trim().isEmpty()){
-            etName.setError("برجاء ادخال اسمك");
+            etName.setError("برجاء ادخال اسم الشركة");
             return;
-        }else if(etPhone.getText().toString().trim().isEmpty()){
-            etName.setError("برجاء ادخال رقم تليفونك");
+        }else if(etEmail.getText().toString().trim().isEmpty()){
+            etEmail.setError("برجاء ادخال البريد الالكترونى");
             return;
         }else {
             // send email
             String body = "الاسم :"+etName.getText().toString()+"\n"
-                    +"رقم تليفون : "+etPhone.getText().toString()+"\n"
-                    +"نوع الطلب :"+requestType.getText().toString()+"\n"
-                    +"تفاصيل الطلب:"+requestDetails.getText().toString()+"\n";
+                    +"العنوان: "+etAddress.getText().toString()+"\n"
+                    +"البريد الالكترونى :"+etEmail.getText().toString()+"\n"
+                    +"نوع الحمولة:"+requestType.getText().toString()+"\n"
+                    +"الوزن:"+size.getText().toString()+"\n"
+                    +"مكان التحميل:"+startpoint.getText().toString()+"\n"
+                    +"مكان النهاية:"+endpoint.getText().toString()+"\n"
+                    +"طرق الدفع:"+pay.getText().toString()+"\n"
+                    +"عدد النقلات:"+number.getText().toString()+"\n"
+                    +"نوع السيارة:"+cartype.getText().toString()+"\n"
+                    +"عدد السيارات:"+numberofcar.getText().toString()+"\n";
             Intent send = new Intent(Intent.ACTION_SENDTO);
             String uriText = "mailto:" + Uri.encode("anwar.abdo1980@gmail.com") +
                     "?subject=" + Uri.encode("نقلتى") +
