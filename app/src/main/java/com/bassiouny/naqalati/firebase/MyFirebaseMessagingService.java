84336a63b3bc4c24bacff62930b812bc840f3373
bassiouny <ahmedbassiouny26@gmail.com>
2017-12-19 22:04:37 +0200

    version 1.14

--- app/src/main/java/com/bassiouny/naqalati/firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/bassiouny/naqalati/firebase/MyFirebaseMessagingService.java
@@ -1,5 +1,10 @@
 package com.bassiouny.naqalati.firebase;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+
+import com.bassiouny.naqalati.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -11,5 +16,19 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        showNotificationAboutNewRequest(remoteMessage.getNotification().getBody());
+    }
+    public void showNotificationAboutNewRequest(String body) {
+        NotificationCompat.Builder n = new NotificationCompat.Builder(this, "default");
+        n.setContentTitle(getString(R.string.app_name));
+        n.setContentText(body);
+        n.setSmallIcon(R.drawable.logo);
+        n.setDefaults(Notification.DEFAULT_SOUND);
+        n.setAutoCancel(true);
+
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, n.build());
     }
 }
